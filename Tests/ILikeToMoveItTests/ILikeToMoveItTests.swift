2386f1d82898377b22369e96d27499bc2aaa345c
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2023-07-07 23:16:50 -0400

    Fixed error in example test

--- Tests/ILikeToMoveItTests/ILikeToMoveItTests.swift
+++ Tests/ILikeToMoveItTests/ILikeToMoveItTests.swift
@@ -6,6 +6,5 @@ final class ILikeToMoveItTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ILikeToMoveIt().text, "Hello, World!")
     }
 }
