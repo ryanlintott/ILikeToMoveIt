963291595b0cf2d5785a7bf491b2c1325d10bbdb
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2024-10-03 16:15:21 -0400

    Added an empty PrivacyInfo file

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ILikeToMoveIt"
+            name: "ILikeToMoveIt",
+            resources: [.copy("PrivacyInfo.xcprivacy")]
         ),
         .testTarget(
             name: "ILikeToMoveItTests",
